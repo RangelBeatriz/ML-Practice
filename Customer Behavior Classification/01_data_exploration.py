6fb6e3203b03287de8922bf75703ab62f8ddb7a1
Beatriz Rangel <srangel.beatriz@gmail.com>
2024-10-08 22:57:50 -0300

    Generating plot figures.

--- Customer Behavior Classification/01_data_exploration.py
+++ Customer Behavior Classification/01_data_exploration.py
@@ -15,3 +15,21 @@ print(df.describe())
 
 
 df["hour"] = df.hour.str.slice(1, 3).astype(int)
+
+#Creating a dataframe with the values to plot
+df_plot = df.copy().drop(columns = ['user', 'screen_list', 'enrolled_date',
+                                           'first_open', 'enrolled'])
+
+
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(1, df_plot.shape[1] + 1):
+    plt.subplot(3, 3, i)
+    f = plt.gca()
+#    f.axes.get_yaxis().set_visible(False)
+    f.set_title(df_plot.columns.values[i - 1])
+
+    vals = np.size(df_plot.iloc[:, i - 1].unique())
+    
+    plt.hist(df_plot.iloc[:, i - 1], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.savefig(path + '/figures/app_data_hist.png')
