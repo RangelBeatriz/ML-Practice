3f06c1145ab010e82ff9bc9ca1747d06cd18223f
Beatriz Rangel <srangel.beatriz@gmail.com>
2024-10-08 22:54:02 -0300

    Data exploration.

--- Customer Behavior Classification/01_data_exploration.py
+++ Customer Behavior Classification/01_data_exploration.py
@@ -3,4 +3,15 @@ import numpy as np
 from dateutil import parser
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
+path = os.getcwd()
+
+df = pd.read_csv(path + '/data/appdata10.csv')
+
+print(df.head())
+
+print(df.describe())
+
+
+df["hour"] = df.hour.str.slice(1, 3).astype(int)
